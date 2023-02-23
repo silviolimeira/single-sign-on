3548d38ac4dcab0a417036dd85fd7ccb76859a16
Silvio Limeira <sicemal@gmail.com>
2023-02-23 09:59:20 -0400

    p2

--- src/main/java/com/limeira/demo3/configuration/SecurityConfiguration.java
+++ src/main/java/com/limeira/demo3/configuration/SecurityConfiguration.java
@@ -2,9 +2,13 @@ package com.limeira.demo3.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,16 +16,24 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
 	@Bean
+	UserDetailsService userDetailsService() {
+		var admin = User.builder().username("cesar").password("123456").roles("USER", "ADMIN").build();
+		var user = User.builder().username("silvio").password("123456").roles("USER").build();
+
+		return new InMemoryUserDetailsManager(admin, user);
+
+	}
+
+	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authConfig ->{
-			authConfig.requestMatchers("/").permitAll();
-			authConfig.requestMatchers("/user/**").authenticated();
-			authConfig.requestMatchers("/admin/**").denyAll();
-		})
-		.formLogin(Customizer.withDefaults())
-		.httpBasic(Customizer.withDefaults());
-		
+		http.authorizeHttpRequests(authConfig -> {
+			authConfig.requestMatchers(HttpMethod.GET, "/").permitAll();
+			authConfig.requestMatchers(HttpMethod.GET, "/user").hasRole("USER");
+			authConfig.requestMatchers(HttpMethod.GET, "/admin").hasRole("ADMIN");
+		}).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
+
 		return http.build();
 	}
-	
+
+
 }
