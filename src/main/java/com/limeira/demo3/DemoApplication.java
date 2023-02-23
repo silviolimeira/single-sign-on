036165267553071fc531bcd6baa7594a1f4755cd
Silvio Limeira <sicemal@gmail.com>
2023-02-23 12:13:18 -0400

    Spring Boot 3 Security (part 03) Jdbc User Details Manager

--- src/main/java/com/limeira/demo3/DemoApplication.java
+++ src/main/java/com/limeira/demo3/DemoApplication.java
@@ -1,13 +1,37 @@
 package com.limeira.demo3;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@Bean
+	CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager, DataSource dataSource) {
+		return args -> {
+			UserDetails user = User.builder().username("user")
+					.password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW").roles("USER")
+					.build();
+			UserDetails admin = User.builder().username("admin")
+					.password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+					.roles("USER", "ADMIN").build();
+			JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+			users.createUser(user);
+			
+			users.createUser(admin);
+		};
 	}
 
 }
--- src/main/java/com/limeira/demo3/configuration/SecurityConfiguration.java
+++ src/main/java/com/limeira/demo3/configuration/SecurityConfiguration.java
@@ -1,50 +1,51 @@
 package com.limeira.demo3.configuration;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-	
-	// ref: https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/in-memory.html#servlet-authentication-inmemory
-	
-	@Bean
-	public UserDetailsService users() {
-		UserDetails user = User.builder()
-			.username("user")
-			.password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
-			.roles("USER")
-			.build();
-		UserDetails admin = User.builder()
-			.username("admin")
-			.password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
-			.roles("USER", "ADMIN")
-			.build();
-		return new InMemoryUserDetailsManager(user, admin);
-	}
-
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(authConfig -> {
+			authConfig.requestMatchers("/h2-console/*").permitAll();
 			authConfig.requestMatchers(HttpMethod.GET, "/").permitAll();
 			authConfig.requestMatchers(HttpMethod.GET, "/user").hasRole("USER");
 			authConfig.requestMatchers(HttpMethod.GET, "/admin").hasRole("ADMIN");
 			authConfig.anyRequest().authenticated();
-		}).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
+		}).csrf().disable().headers().frameOptions().disable().and().formLogin(Customizer.withDefaults())
+				.httpBasic(Customizer.withDefaults());
 
 		return http.build();
 	}
+
+	@Bean
+	JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		return jdbcUserDetailsManager;
+	}
+
+	// option 1
+	@Bean
+	DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION).build();
+	}
+
 
 
 }
