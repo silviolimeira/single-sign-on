c085bb7e4b5a678f1128d7eaf9686dbce3f5a134
Silvio Limeira <sicemal@gmail.com>
2023-02-23 08:56:02 -0400

    Spring Boot 3 Security (Part 01) Security Config - SecurityFilterChain

---
+++ src/main/java/com/limeira/demo3/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.limeira.demo3.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "<h1>Welcome home!</h1>";
+	}
+
+	@GetMapping("/user")
+	public String user(Authentication authentication) {
+		return "<h1>Welcome " + authentication.getName() + "!<h1>";
+	}
+
+	@GetMapping("/admin")
+	public String admin(Authentication authentication) {
+		return "<h1>Welcome \" + authentication.getName + \"!</h1>";
+	}
+
+}
---
+++ src/test/java/com/limeira/demo3/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limeira.demo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
